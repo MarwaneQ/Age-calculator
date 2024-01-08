1c5ae6ecb4c7169e771199c28443bde54165ac8c
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-01-08 13:56:27 -0800

    adding js

---
+++ index.js
@@ -0,0 +1,21 @@
+let navBar = document.createElement('nav')
+navBar.className = 'NavSection'
+document.body.appendChild(navBar)
+let ul = document.createElement('ul')
+for (let index = 0; index < 3; index++) {
+    let li = document.createElement('li')
+    ul.appendChild(li)
+    li.className = `li-${index}`
+    
+}
+navBar.appendChild(ul)
+
+document.querySelector('.li-0').textContent = 'Home'
+document.querySelector('.li-1').textContent = 'About'
+document.querySelector('.li-2').textContent = 'Contact'
+navBar.style.cssText = `
+background-color: #eee;
+height: 9vh;
+color: black;
+`
+
