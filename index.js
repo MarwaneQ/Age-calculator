a14f2733991aeff67ba9526d3267e9e9a6a9d158
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-01-15 13:51:49 -0800

    t

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-let navBar = document.createElement('nav')
-navBar.className = 'NavSection'
-document.body.appendChild(navBar)
-let ul = document.createElement('ul')
-for (let index = 0; index < 3; index++) {
-    let li = document.createElement('li')
-    ul.appendChild(li)
-    li.className = `li-${index}`
+// let navBar = document.createElement('nav')
+// navBar.className = 'NavSection'
+// document.body.appendChild(navBar)
+// let ul = document.createElement('ul')
+// for (let index = 0; index < 3; index++) {
+//     let li = document.createElement('li')
+//     ul.appendChild(li)
+//     li.className = `li-${index}`
     
-}
-navBar.appendChild(ul)
+// }
+// navBar.appendChild(ul)
 
-document.querySelector('.li-0').textContent = 'Home'
-document.querySelector('.li-1').textContent = 'About'
-document.querySelector('.li-2').textContent = 'Contact'
-navBar.style.cssText = `
-background-color: #eee;
-height: 9vh;
-color: black;
-`
+// document.querySelector('.li-0').textContent = 'Home'
+// document.querySelector('.li-1').textContent = 'About'
+// document.querySelector('.li-2').textContent = 'Contact'
+// navBar.style.cssText = `
+// background-color: #eee;
+// height: 9vh;
+// color: black;
+// `
+// let arr = [10,20,40,60]
+// arr.forEach(function(a){
+//     let c = a*2
+//     console.log(c);
+// })
+
+document.querySelector("button").setAttribute("class","me")
 
